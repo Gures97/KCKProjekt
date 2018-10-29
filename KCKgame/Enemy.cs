f6f52e34d06646b4d53d70d9eb12420dafc01de2
gures97 <19gures97@gmail.com>
2018-10-29 02:49:05 +0100

    Nowy branch

--- KCKgame/Enemy.cs
+++ KCKgame/Enemy.cs
@@ -8,10 +8,42 @@ namespace KCKgame
 {
     class Enemy : Entity
     {
+        enum EnemyState{
+            nothing,
+            follow,
+            attack
+        };
+
+        EnemyState state = EnemyState.nothing;
+        int PlayerX;
+        int PlayerY;
+        bool ready = false;
+        int vision = 5;
         public Enemy()
         {
             ChangeVisual(":(");
 
         }
+
+        public void SetPlayerCoordinates(int x, int y)
+        {
+            PlayerX = x;
+            PlayerY = y;
+        }
+
+        public void EnemyMovement()
+        {
+            //krok co dwa ticki
+            if (ready)
+            {
+                //if(state = EnemyState.nothing) - szukaj przeciwnika
+                //if(state = EnemyState.follow) - zblizaj sie do przeciwnika
+                //if(state = EnemyState.attack) - zadaj obrazenia
+                ready = false;
+            }
+            else
+                ready = true;
+        }
+
     }
 }
